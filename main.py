df45aade1bff11a4bd5c34e4563f728e9f68beb7
Leonard develops <120578632+leonard-roepcke@users.noreply.github.com>
2025-10-06 10:10:44 +0000

    gitignore key test

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+from github import Github
 print("Starting Automated Github-Repository-Sync")
+
+from dotenv import load_dotenv
+import os
+
+# Pfad zur .env Datei laden
+load_dotenv(dotenv_path=".env/.env")
+
+# Token aus der Umgebungsvariable auslesen
+token = os.getenv("github_key")
+
+git = Github(token)
+
+print(token)
+print(git)
